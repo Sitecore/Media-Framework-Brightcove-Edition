18187a56db3613b79212ba2455e563f2c3ae22f9
Uy Tran <uytran@gmail.com>
2018-04-23 13:21:42 -0700

    force sync clear end pipeline

--- src/Brightcove.MediaFramework.Brightcove/Import/VideoCollectionImporterForce.cs
+++ src/Brightcove.MediaFramework.Brightcove/Import/VideoCollectionImporterForce.cs
@@ -16,7 +16,7 @@ namespace Brightcove.MediaFramework.Brightcove.Import
         /// </summary>
         public override Entities.Collections.PagedCollection<Video> RetrieveList(int limit, int offset, IAuthenticator authenticator)
         {
-            HttpRuntime.Cache.Insert("BrightcoveForceSync", true, null, System.Web.Caching.Cache.NoAbsoluteExpiration, TimeSpan.FromMinutes(3));
+            HttpRuntime.Cache.Insert("BrightcoveForceSync", true, null, System.Web.Caching.Cache.NoAbsoluteExpiration, TimeSpan.FromMinutes(60));
             return new VideoProxy(authenticator).RetrieveList(limit, offset);
         }
     }
